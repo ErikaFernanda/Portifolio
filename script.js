b4130e32dd2bdc20c024347da65c9de59142c649
Erika Fernanda <erikasolivee@gmail.com>
2021-08-17 08:59:18 -0300

    fazendo animação

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-let toggle = document.querySelector(".hamburger");
-let sidebar = document.querySelector(".sidebar");
-let userImage = document.querySelector(".user");
+// let toggle = document.querySelector(".hamburger");
+// let sidebar = document.querySelector(".sidebar");
+// let userImage = document.querySelector(".user");
 
-toggle.addEventListener("click", () => {
-  userImage.classList.toggle("fade");
-});
+// toggle.addEventListener("click", () => {
+//   userImage.classList.toggle("fade");
+// });
 
-function toggleMenu() {
-  toggle.classList.toggle("active");
-  sidebar.classList.toggle("active");
-}
+// function toggleMenu() {
+//   toggle.classList.toggle("active");
+//   sidebar.classList.toggle("active");
+// }
+
+// const saida =document.querySelector(".line")
+// function digitacao(texto , contador){
+//   if(contador<texto.length){
+//     setTimeout(()=>{
+//       saida.textContent= texto.charAt(contador);
+//       contador++;
+//       digitacao(texto,contador);
+//     },90)
+//   }
+// }
+// digitacao("Sempre em busca de conhecimento tecnológico no mundo da musica , fotografia ,jogos , robótica. Mas existe uma atenção redobrada para a programação , mas precisamente na linguagem JAVA.",0);
